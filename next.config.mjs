6e5081aa68d58e2edd583ce5156665a26adf12a3
claudio alberto xool castillo <Elhuevinn>
2025-10-03 18:39:53 -0500

    feat: configurar Next.js en modo stanlone para Azure App Service

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Esta línea es crucial para despliegues en contenedores como App Service
+  output: 'standalone', 
+  // ... otras configuraciones que ya tenías
+};
 
 export default nextConfig;
